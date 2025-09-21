3e404c95a02d5c07476e8bfe274501c009c41eb9
zoomdong <1344492820@qq.com>
2025-09-21 22:11:55 +0800

    chore: enable wasm

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,8 @@ import { trim } from '@/utils/format';
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
 import styles from './index.less';
+// import assetsBpmn from '@/assets/assets.bpmn';
+import assetsWasm from '@/assets/assets.wasm';
 
 const HomePage: React.FC = () => {
   const { name } = useModel('global');
@@ -10,6 +12,7 @@ const HomePage: React.FC = () => {
     <PageContainer ghost>
       <div className={styles.container}>
         <Guide name={trim(name)} />
+        <video src={assetsWasm}></video>
       </div>
     </PageContainer>
   );
