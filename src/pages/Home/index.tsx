da729165a80872dd978f751e43b2e648f3806c41
zoomdong <1344492820@qq.com>
2025-10-28 09:29:02 +0800

    chore: bump @utoo/pack to 0.0.1-alpha.57

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,16 @@ import Guide from '@/components/Guide';
 import { trim } from '@/utils/format';
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
+import './global2.less';
 import styles from './index.less';
+
+export * from './type.d';
 
 const HomePage: React.FC = () => {
   const { name } = useModel('global');
   return (
     <PageContainer ghost>
+      <div className=".ant-checkbox-wrapper">test style</div>
       <div className={styles.container}>
         <Guide name={trim(name)} />
       </div>
