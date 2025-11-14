a9ff3ef6fc7c57def0827e29cb8cb38e0a0b11ae
zoomdong <1344492820@qq.com>
2025-11-14 15:55:54 +0800

    chore: bump utoopack

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,11 @@ import Guide from '@/components/Guide';
 import { trim } from '@/utils/format';
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
-import './global2.less';
-import styles from './index.less';
+// import './global2.less';
+// import styles from './index.less';
 // import path from 'path';
 // import { z } from 'test-z';
+import './index.less';
 
 // z
 export * from './type.d';
@@ -23,7 +24,7 @@ const HomePage: React.FC = () => {
     <PageContainer ghost>
       <div className="ant-checkbox-wrapper">test style</div>
       <div className="ant-menu">test style2</div>
-      <div className={styles.container}>
+      <div>
         <Guide name={trim(name)} />
       </div>
     </PageContainer>
